45fe801d6c9ac4b7238d6020d1a467b9dea915e0
Grant Fowler <fowler64@icloud.com>
2016-07-29 18:40:52 -0400

    set up mealviewcontroller and created actions and outlets

---
+++ CollegeMealPlanner/CollegeMealPlanner/Meal.swift
@@ -0,0 +1,9 @@
+//
+//  Meal.swift
+//  CollegeMealPlanner
+//
+//  Created by Grant Fowler on 7/29/16.
+//  Copyright © 2016 Grant Fowler. All rights reserved.
+//
+
+import Foundation
---
+++ CollegeMealPlanner/CollegeMealPlanner/MealViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MealViewController.swift
+//  CollegeMealPlanner
+//
+//  Created by Grant Fowler on 7/29/16.
+//  Copyright © 2016 Grant Fowler. All rights reserved.
+//
+
+import UIKit
+
+class MealViewController: UIViewController {
+    //MARK: Outlets
+    
+    @IBOutlet weak var mealCounter: UISegmentedControl!
+
+    @IBOutlet weak var diningText: UITextField!
+    
+    @IBOutlet weak var restaurantText: UITextField!
+    
+    
+    
+    
+    //MARK: Actions
+    @IBAction func finishMeal(sender: AnyObject) {
+    }
+    
+//    //MARK: Stuff for admob ad
+//    @IBOutlet weak var bannerView: GADBannerView!
+//    
+//    func adFunction(){
+//        bannerView.adUnitID = "ca-app-pub-4551183276117497/9211445162"
+//        bannerView.rootViewController = self
+//        let request: GADRequest = GADRequest()
+//        
+//        
+//        // for actual ad comment next line
+//        request.testDevices = [kGADSimulatorID]
+//        
+//        
+//        bannerView.loadRequest(request)
+//    }
+}
