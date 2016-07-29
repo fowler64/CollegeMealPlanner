c5504833ae415ac80c9d5decdf363ebef38f8e34
Grant Fowler <fowler64@icloud.com>
2016-07-29 13:33:17 -0400

    initial setup

---
+++ CollegeMealPlanner/CollegeMealPlanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollegeMealPlanner
+//
+//  Created by Grant Fowler on 7/29/16.
+//  Copyright © 2016 Grant Fowler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
