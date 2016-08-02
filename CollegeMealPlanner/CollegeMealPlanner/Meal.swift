ac50efaf7230ae9cfafeef48c06e49376f4fccea
Grant Fowler <fowler64@icloud.com>
2016-08-02 15:11:32 -0400

    added dates to meals and changed dining to int (multiplied by 100) to prevent displaying huge decimals and rounding errors

--- CollegeMealPlanner/CollegeMealPlanner/Meal.swift
+++ CollegeMealPlanner/CollegeMealPlanner/Meal.swift
@@ -8,15 +8,28 @@
 
 import Foundation
 
+struct Keys {
+    static let name = "nameKey"
+    static let meal = "mealKey"
+    static let restaurant = "restaurantKey"
+    static let dateComp = "dateComponentKey"
+    static let date = "dateKey"
+}
+
 class Meal {
     var meals: Int
-    var dining: Double?
+    var dining: Int?
     var restaurant: String?
+    var dateComp = NSDateComponents()
+    var date = NSDate()
     
     init(meals: Int, dining: Double?, restaurant: String?){
         self.meals = meals
-        self.dining = dining
         self.restaurant = restaurant
+        if dining != nil{
+            self.dining = Int(dining! * 100)
+        }
         print("created new meal with values \(self.meals) \(self.dining) \(self.restaurant)")
+        print("on " + date.description)
     }
 }
